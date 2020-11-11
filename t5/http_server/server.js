14b77d79f41eb4b96227447f39fd3edd1c57e941
Vasco Ramos <vascoalramos@outlook.pt>
2020-11-11 17:43:23 +0000

    Add course detail page

--- t5/http_server/server.js
+++ t5/http_server/server.js
@@ -3,81 +3,6 @@ const axios = require("axios");
 
 axios.defaults.baseURL = "http://localhost:3000";
 
-const listStudents = (res) => {
-	axios
-		.get("/alunos")
-		.then((resp) => {
-			let alunos = resp.data;
-
-			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-			res.write("<h2>Escola de Música: Lista de Alunos</h2>");
-			res.write("<ul>");
-
-			alunos.forEach((aluno) => {
-				res.write(`<li>${aluno.id} - ${aluno.nome}</li>`);
-			});
-
-			res.write("</ul>");
-			res.write(`<address>[<a href="/">Voltar à Home</a>]</address>`);
-			res.end();
-		})
-		.catch((err) => {
-			console.log(`Èrro na obtenção da lista de alunos: ${err}`);
-		});
-};
-
-const listCourses = (res) => {
-	axios
-		.get("/cursos")
-		.then((resp) => {
-			let cursos = resp.data;
-
-			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-			res.write("<h2>Escola de Música: Lista de Cursos</h2>");
-			res.write("<ul>");
-
-			cursos.forEach((curso) => {
-				res.write(`<li>${curso.id} - ${curso.designacao}</li>`);
-			});
-
-			res.write("</ul>");
-			res.write(`<address>[<a href="/">Voltar à Home</a>]</address>`);
-			res.end();
-		})
-		.catch((err) => {
-			console.log(`Èrro na obtenção da lista de alunos: ${err}`);
-		});
-};
-
-const listInstruments = (res) => {
-	axios
-		.get("/instrumentos")
-		.then((resp) => {
-			let instrumentos = resp.data;
-
-			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-			res.write("<h2>Escola de Música: Lista de Instrumentos</h2>");
-			res.write("<ul>");
-
-			instrumentos.forEach((instr) => {
-				res.write(`<li>${instr.id} - ${instr["#text"]}</li>`);
-			});
-
-			res.write("</ul>");
-			res.write(`<address>[<a href="/">Voltar à Home</a>]</address>`);
-			res.end();
-		})
-		.catch((err) => {
-			console.log(`Èrro na obtenção da lista de alunos: ${err}`);
-		});
-};
-
-const requestNotSupported = (res) => {
-	res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-	res.write(`<p>Pedido não suportado: ${req.method} ${req.url}</p>`);
-	res.end();
-};
-
 http.createServer((req, res) => {
 	console.log(`${req.method} ${req.url}`);
 
@@ -99,12 +24,141 @@ http.createServer((req, res) => {
 			listCourses(res);
 		} else if (req.url === "/instrumentos") {
 			listInstruments(res);
+		} else if (req.url.match(/\/alunos\/\w+/)) {
+			const studentId = req.url.slice(8);
+			getStudentInfo(res, studentId);
+		} else if (req.url.match(/\/cursos\/\w+/)) {
+			const courseId = req.url.slice(8);
+			getCourseInfo(res, courseId);
 		} else {
-			requestNotSupported(res);
+			requestNotSupported(req, res);
 		}
 	} else {
-		requestNotSupported(res);
+		requestNotSupported(req, res);
 	}
 }).listen(4000);
 
 console.log("Server listening at port 4000...");
+
+const listStudents = (res) => {
+	axios
+		.get("/alunos")
+		.then((resp) => {
+			let students = resp.data;
+
+			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+			res.write("<h2>Escola de Música: Lista de Alunos</h2>");
+			res.write("<ul>");
+
+			students.forEach((s) => {
+				res.write(`<li><a href="/alunos/${s.id}">${s.id} - ${s.nome}</a></li>`);
+			});
+
+			res.write("</ul>");
+			res.write(`<address>[<a href="/">Voltar à Home</a>]</address>`);
+			res.end();
+		})
+		.catch((err) => {
+			console.log(`Erro na obtenção da lista de alunos: ${err}`);
+		});
+};
+
+const listCourses = (res) => {
+	axios
+		.get("/cursos")
+		.then((resp) => {
+			let courses = resp.data;
+
+			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+			res.write("<h2>Escola de Música: Lista de Cursos</h2>");
+			res.write("<ul>");
+
+			courses.forEach((c) => {
+				res.write(`<li><a href="cursos/${c.id}">${c.id} - ${c.designacao}<a></li>`);
+			});
+
+			res.write("</ul>");
+			res.write(`<address>[<a href="/">Voltar à Home</a>]</address>`);
+			res.end();
+		})
+		.catch((err) => {
+			console.log(`Erro na obtenção da lista de cursos: ${err}`);
+		});
+};
+
+const listInstruments = (res) => {
+	axios
+		.get("/instrumentos")
+		.then((resp) => {
+			let instruments = resp.data;
+
+			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+			res.write("<h2>Escola de Música: Lista de Instrumentos</h2>");
+			res.write("<ul>");
+
+			instruments.forEach((i) => {
+				res.write(`<li>${i.id} - ${i["#text"]}</li>`);
+			});
+
+			res.write("</ul>");
+			res.write(`<address>[<a href="/">Voltar à Home</a>]</address>`);
+			res.end();
+		})
+		.catch((err) => {
+			console.log(`Erro na obtenção da lista de instrumentos: ${err}`);
+		});
+};
+
+const getStudentInfo = (res, studentId) => {
+	axios
+		.get(`/alunos/${studentId}`)
+		.then((resp) => {
+			let student = resp.data;
+
+			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+			res.write(`<h2>Escola de Música: Aluno ${student.id}</h2>`);
+
+			res.write(`<p><b>ID: </b>${student.id}<p>`);
+			res.write(`<p><b>NOME: </b>${student.nome}<p>`);
+			res.write(`<p><b>NASCIMENTO: </b>${student.dataNasc}<p>`);
+			res.write(`<p><b>CURSO: </b>${student.curso}<p>`);
+			res.write(`<p><b>ANO DO CURSO: </b>${student.anoCurso}<p>`);
+			res.write(`<p><b>INSTRUMENTO: </b>${student.instrumento}<p>`);
+
+			res.write(`<address>[<a href="/">Voltar à Home</a>]</address>`);
+			res.end();
+		})
+		.catch((err) => {
+			console.log(`Erro na obtenção do aluno: ${err}`);
+		});
+};
+
+const getCourseInfo = (res, courseId) => {
+	axios
+		.get(`/cursos/${courseId}`)
+		.then((resp) => {
+			let course = resp.data;
+
+			res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+			res.write(`<h2>Escola de Música: Aluno ${course.id}</h2>`);
+
+			res.write(`<p><b>ID: </b>${course.id}<p>`);
+			res.write(`<p><b>DESIGNAÇÃO: </b>${course.designacao}<p>`);
+			res.write(`<p><b>DURAÇÂO: </b>${course.duracao} anos<p>`);
+			res.write(
+				`<p><b>INSTRUMENTO: </b><a href="instrumentos/${course.instrumento.id}">${course.instrumento["#text"]}</a><p>`,
+			);
+
+			res.write(`<address>[<a href="/">Voltar à Home</a>]</address>`);
+			res.end();
+		})
+		.catch((err) => {
+			console.log(`Erro na obtenção do curso: ${err}`);
+		});
+};
+
+const requestNotSupported = (req, res) => {
+	res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+	res.write(`<p>Pedido não suportado: ${req.method} ${req.url}</p>`);
+	res.end();
+};
